8df3cc7b6168f2a7d8c52b6aeff63446715a3d7d
VulcanCCIT <chuckcondron@chuckcondron.com>
2025-10-03 14:03:56 -0700

    Fixed errors

--- KeyBoardView.swift
+++ KeyBoardView.swift
@@ -111,7 +111,7 @@ struct KeyBoardView: View {
           let midi = pitch.intValue
           let externallyOn = conductor.activeNotes.contains(midi)
           //let externalIntensity = externalVelocities[midi] ?? 0.0
-          let isBlack = [1, 3, 6, 8, 10].contains(midi % 12)
+          //let isBlack = [1, 3, 6, 8, 10].contains(midi % 12)
           //let overlayColor: Color = isBlack ? .cyan : .blue
           ZStack {
             KeyboardKey(
